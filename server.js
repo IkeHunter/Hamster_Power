0ced8fb63401962b61543d2493db2bbf625db0a0
Isaac Hunter <isaachunter@Isaacs-MacBook-Pro.local>
2019-05-23 20:51:05 -0400

    bug fix

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 3000);
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname + './dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
